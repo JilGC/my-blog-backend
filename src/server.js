43d4227e815c647eb95d0e37731be80898f29cc9
JilGC <105502236+JilGC@users.noreply.github.com>
2024-05-27 00:02:25 -0300

    recent changes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
 
+import { connectToDb, db } from './db.js';
 
 const app = express();
 
@@ -9,10 +9,7 @@ app.use(express.json());
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
 
-    const db = client.db('react-blog-db');
 
     const article = await db.collection('articles').findOne({ name });//to use specific id
 
@@ -30,10 +27,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
     // const article = articlesInfo.find(a => a.name === name);
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
 
-    const db = client.db('react-blog-db');
 
     db.collection('articles').updateOne({ name }, {
         $inc: { upvotes: 1 },
@@ -54,16 +48,13 @@ app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { postedBy, text } = req.body;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
 
-    const db = client.db('react-blog-db');
 
     await db.collection('articles').updateOne({ name }, {
         $push: { comments: { postedBy, text } },
     })
 
-
+    const article = await db.collection('articles').findOne({ name });
 
     if (article) {
         res.send(article.comments);
@@ -74,6 +65,9 @@ app.post('/api/articles/:name/comments', async (req, res) => {
 
 })
 
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
-});
+connectToDb(() => {
+    console.log('Successfully connected to database!!')
+    app.listen(8000, () => {
+        console.log("Server is listening on port 8000");
+    });
+})
